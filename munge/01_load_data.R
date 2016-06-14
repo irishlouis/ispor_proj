0165f4abba138a644955e5a653a58ce7f0edf039
irishlouis <louissmith@gmail.com>
2016-06-14 17:29:56 +0100

    strip down and call to functions

--- munge/01_load_data.R
+++ munge/01_load_data.R
@@ -1,50 +1,14 @@
 # get csv files from /data
-csv.files <- paste0("data/", list.files("data"))
 
-# get the summary files
-epoch <- fread(csv.files[str_detect(csv.files, "summary")], header = TRUE, stringsAsFactors = FALSE)
-epoch <- epoch[,':='(
-  timestamp = ymd_hms(timestamp))][,
-                                   .(timestamp, serialnumber, steps),][,
-                                                                       steps :=bin_steps(steps),]
-setkey(epoch, serialnumber, timestamp)
+# load training / testing dataset 
+## data from louis and triona and 1 dog
+train.data <- load.data(datafolder = "louis1")
+cache("train.data")
 
-# empty list to hold raw data
-data <- list()
-# allow for milliseconds
-options(digits.secs=3)
 
-for (i in csv.files[str_detect(csv.files, "RAW.csv")]){
-  dt <- fread(i, header = T, stringsAsFactors = F)
-  
-  device_id <- strsplit(strsplit(i, " ")[[1]][1], "/")[[1]][2]
-  dt[,':='(
-    device_id = device_id,
-    datetime = dmy_hms(Timestamp),
-    Timestamp = NULL,
-    vec.mag = sqrt(`Accelerometer X`^2 + `Accelerometer Y`^2 + `Accelerometer Z`^2),
-    `Accelerometer X` = NULL,
-    `Accelerometer Y` = NULL,
-    `Accelerometer Z` = NULL
-  ),][,':='(
-      time_minute = floor_date(datetime, "minute"),
-      second = 5 * floor(second(datetime)/5)
-      ),][,':='(
-        epoch_id = time_minute + seconds(second),
-        time_minute = NULL,
-        second = NULL
-      ),]
-  
-  data[[which(csv.files[str_detect(csv.files, "RAW.csv")] == i)]] <- dt
-}
-rm(dt)
-# collapse to single file
-data <- rbindlist(data)
-setkey(data, device_id, epoch_id)
 
-# join epoch steps to raw
-data <- data[epoch] 
-rm(epoch)
 
-# cache
-cache("data")
+# load final evaluation dataset for unseen subjects
+## data from marie and 2 dogs
+eval2.data <- load.data(datafolder = "marie")
+cache("eval2.data")
--- munge/02_summarise_data.R
+++ munge/02_summarise_data.R
@@ -1,23 +1,20 @@
 
-# create summary data set 
-p <- proc.time()
-# get summary data for vector magnitude
-vector.summary <- data[(epoch_id >= ymd_hms("2016-05-04 18:30:00") & 
-                          epoch_id < ymd_hms("2016-05-04 18:55:00")), 
-     get.peak.summary(vec.mag, k = 25, freq = 100), 
-     .(device_id, epoch_id) ] %>% setkey(device_id, epoch_id)
-# get summary data for peak summary
-peak.summary <- data[(epoch_id >= ymd_hms("2016-05-04 18:30:00") & 
-                        epoch_id < ymd_hms("2016-05-04 18:55:00")), 
-     .(avg.vec = mean(vec.mag),
-       sd.vec = sd(vec.mag),
-       steps = max(steps)), 
-     .(device_id, epoch_id) ] %>% setkey(device_id, epoch_id)
-(proc.time() - p)[3]
+# summarise maries evalusation data
+train.summary <- summarise.data(data = train.data[device_id != "TAS1E31150005"], 
+                                human.devices = c("TAS1E31150003", "TAS1E31150028"))
+## filter to walk period
+train.summary <- train.summary[epoch_id >= ymd_hms("2016-05-04 18:30:00") & 
+                                  epoch_id < ymd_hms("2016-05-04 18:55:00")]
+cache("train.summary")
 
-# join summary data
-summary <- vector.summary[peak.summary]
 
-# add label for human / dog
-summary[,subj_type:=ifelse(model.data2$device_id %in% c("TAS1E31150059"), 
-                           "dog", "human"),]
+
+
+
+
+# summarise maries evalusation data
+eval2.summary <- summarise.data(data = eval2.data, human.devices = "TAS1E31150005")
+## filter to walk period
+eval2.summary <- eval2.summary[epoch_id > ymd_hms("20160614 064500") & 
+                                 epoch_id < ymd_hms("20160614 070030")]
+cache("eval2.summary")
