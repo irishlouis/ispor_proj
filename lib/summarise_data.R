6727cfa8927b9c442812866bda2dbd43ce1b09d6
irishlouis <louissmith@gmail.com>
2016-06-15 17:01:25 +0100

    changes for step_bin

--- lib/summarise_data.R
+++ lib/summarise_data.R
@@ -13,15 +13,17 @@ summarise.data <- function(data, human.devices){
   p <- proc.time()
   # get summary data for vector magnitude
   ## exclude epochs with low step data
-  setkey(data, device_id, epoch_id, steps)
-  vector.summary <- data[steps != "0-2", 
+  setkey(data, device_id, epoch_id, steps_bin)
+  data <- data[complete.cases(data)]
+  vector.summary <- data[steps_bin != "0-2", 
                          .(avg.vec = mean(vec.mag),
                            sd.vec = sd(vec.mag),
-                           steps = steps), 
+                           steps = steps,
+                           steps_bin = steps_bin), 
                          .(device_id, epoch_id) ] %>% setkey(device_id, epoch_id)
   
   # get summary data for peak summary
-  peak.summary <- data[steps != "0-2", 
+  peak.summary <- data[steps_bin != "0-2", 
                        get.peak.summary(vec.mag, k = 25, freq = 100), 
                        .(device_id, epoch_id) ] %>% setkey(device_id, epoch_id)
   (proc.time() - p)[3]
