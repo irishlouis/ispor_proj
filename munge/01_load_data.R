3d2e27aec8398ebbc0ee196582486b7e59702ee8
irishlouis <louissmith@gmail.com>
2016-06-15 17:02:01 +0100

    first evaluation data, same subjs, different date

--- munge/01_load_data.R
+++ munge/01_load_data.R
@@ -5,8 +5,10 @@
 train.data <- load.data(datafolder = "louis1")
 cache("train.data")
 
-
-
+# load eval dataset 
+## data from louis and triona and 1 dog
+eval1.data <- load.data(datafolder = "louis2")
+cache("eval1.data")
 
 # load final evaluation dataset for unseen subjects
 ## data from marie and 2 dogs
--- munge/02_summarise_data.R
+++ munge/02_summarise_data.R
@@ -1,5 +1,5 @@
 
-# summarise maries evalusation data
+# summarise test/training data
 train.summary <- summarise.data(data = train.data[device_id != "TAS1E31150005"], 
                                 human.devices = c("TAS1E31150003", "TAS1E31150028"))
 ## filter to walk period
@@ -8,8 +8,14 @@ train.summary <- train.summary[epoch_id >= ymd_hms("2016-05-04 18:30:00") &
 cache("train.summary")
 
 
-
-
+# summarise evaluation data
+## same subjects as train
+eval1.summary <- summarise.data(data = eval1.data, 
+                                human.devices = c("TAS1E31150030", "TAS1E35150289"))
+## filter to walk period
+eval1.summary <- eval1.summary[epoch_id >= ymd_hms("2016-06-15 07:53:00") & 
+                                 epoch_id < ymd_hms("2016-06-15 08:22:00")]
+cache("eval1.summary")
 
 
 # summarise maries evalusation data
