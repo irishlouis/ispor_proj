06a2ee9ea5470be2f9ed5e6dcdf90b15cde979ce
irishlouis <louissmith@gmail.com>
2016-06-15 17:04:07 +0100

    change to final_model

--- src/05_eval_mdl_unseen.R
+++ src/05_eval_mdl_unseen.R
@@ -1,4 +1,9 @@
+# test model on same subjects different data
+confusionMatrix(predict(final_model, eval1.summary), eval1.summary$subj_type)
+
+
 # test model on different subjects
-confusionMatrix(predict(final_svm, eval2.summary), eval2.summary$subj_type)
+confusionMatrix(predict(final_model, eval2.summary), eval2.summary$subj_type)
+
 
 
