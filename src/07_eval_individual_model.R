c5886a8bc74f8cbbf4e39ca88d1ea176f9efb7d4
irishlouis <louissmith@gmail.com>
2016-10-25 15:49:42 +0100

    tidying text

--- src/07_eval_individual_model.R
+++ src/07_eval_individual_model.R
@@ -1,7 +1,7 @@
 # test model on same subjects different data
 table(predict(final_model_id, eval1.summary), eval1.summary$device_id )
-confusionMatrix(predict(init_model_id,eval1.summary), ifelse(eval1.summary$device_id == "TAS1E35150289", "louis", "not_louis"))
-confusionMatrix(predict(final_model_id,eval1.summary), ifelse(eval1.summary$device_id == "TAS1E35150289", "louis", "not_louis"))
+confusionMatrix(predict(init_model_id, eval1.summary), ifelse(eval1.summary$device_id == "TAS1E35150289", "louis", "not_louis"))
+confusionMatrix(predict(final_model_id, eval1.summary), ifelse(eval1.summary$device_id == "TAS1E35150289", "louis", "not_louis"))
 
 # test model on different subjects
 table(predict(final_model_id, eval2.summary), eval2.summary$device_id)
