646f999029fb738c92669cde81c34bd87f060fa0
irishlouis <louissmith@gmail.com>
2016-10-26 09:01:47 +0100

    add additional model validation

--- src/10_train_first_6min.R
+++ src/10_train_first_6min.R
@@ -23,15 +23,22 @@ init_control <- trainControl(
 
 # train list of models using control spec'd above
 set.seed(456798)
-first6_model <- train(subj_type ~ ., 
+first6_model_type <- train(subj_type ~ ., 
                     data = training, 
                     trControl = init_control,
                     metric = "Kappa",
                     method = "nnet",
                     tuneLength = 10)
 # The final values used for the model were size = 5 and decay = 0.006772878
-varImp(first6_model)
-plot(varImp(first6_model))
+varImp(first6_model_type)
+plot(varImp(first6_model_type))
 
 # look at validation results 
-confusionMatrix(predict(first6_model, testing), testing$subj_type)
+confusionMatrix(predict(first6_model_type, testing), testing$subj_type)
+cache("first6_model_type")
+
+# beyond test data
+## same subjects different day
+confusionMatrix(predict(first6_model_type, eval1.summary), eval1.summary$subj_type)
+## different subjects different day
+confusionMatrix(predict(first6_model_type, eval2.summary), eval2.summary$subj_type)
